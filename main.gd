684222cf00ae402d56251c3cfc0f599ba6096cce
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-17 04:36:53 -0500

    [FEATURE] Basic interface

---
+++ main.gd
@@ -0,0 +1,102 @@
+extends Control
+
+## Default time label
+const NULL_TIMER := "[center]--:--[/center]"
+
+## Number of seconds notification lasts
+const NOTIFICATION_TIMEOUT := 60_000
+
+@onready var work_timer: Timer = $WorkTimer
+@onready var break_timer: Timer = $BreakTimer
+@onready var break_timer_long: Timer = $BreakTimerLong
+@onready var timer_label: RichTextLabel = $MarginContainer/MarginContainer/VBoxContainer/TimerLabel
+@onready var alarm: AudioStreamPlayer = $Alarm
+
+var _timer_type := "work"
+@onready var _current_timer: Timer = work_timer
+
+func _ready() -> void:
+	_on_timer_type_changed(true, "work")
+	_initialize_timers()
+
+func _process(_delta: float) -> void:
+	_update_timer_label()
+
+func _initialize_timers():
+	# Connect timers' timeout signal
+	var temp = [
+		{
+			node = work_timer,
+			name = "work"
+		},
+		{
+			node = break_timer,
+			name = "break"
+		},
+		{
+			node = break_timer_long,
+			name = "break_long"
+		}
+	]
+	for d in temp:
+		var node: Timer = d.node
+		var word: String = d.name
+		print_debug("%s timeout connected with word '%s'" % [node, word])
+		node.timeout.connect(_on_work_timer_timeout.bind(word))
+
+func _update_timer_label() -> void:
+	if _current_timer.paused or _current_timer.is_stopped():
+		return
+	# Format label to MM:SS.ms
+	var time = seconds_to_time(_current_timer.time_left)
+	var seconds: float = snappedf(time.seconds, 0.01)
+	timer_label.text = "[center]%02d:%05.2f[/center]" % [time.minutes, seconds]
+
+func os_notify(title: String, message: String) -> void:
+	OS.execute("notify-send", ["-t", NOTIFICATION_TIMEOUT, title, message])
+
+func seconds_to_time(sec: float) -> Dictionary:
+	var minutes := int(sec / 60.0)
+	return {
+		minutes = minutes,
+		seconds = sec - float(minutes) * 60.0
+	}
+
+# Signals
+
+func _on_start_timer_pressed() -> void:
+	if _current_timer.is_stopped():
+		_current_timer.start()
+
+func _on_timer_type_changed(toggled_on: bool, type: String) -> void:
+	_on_stop_timer_pressed()
+	if toggled_on:
+		_timer_type = type
+		match type:
+			"work":
+				_current_timer = work_timer
+			"short":
+				_current_timer = break_timer
+			"long":
+				_current_timer = break_timer_long
+			_:
+				push_error("Unknown type '%s'" % type)
+
+func _on_stop_timer_pressed() -> void:
+	_current_timer.stop()
+	timer_label.text = NULL_TIMER
+
+func _on_pause_timer_pressed() -> void:
+	_current_timer.paused = ! _current_timer.paused
+
+@warning_ignore("shadowed_variable_base_class")
+func _on_work_timer_timeout(word: String) -> void:
+	alarm.play()
+	_update_timer_label()
+	match word:
+		"work":
+			os_notify("Work Over!", "Time to take a break!")
+		"break":
+			os_notify("Break Over!", "Get back to work, slave! Wahahaha!")
+		"break_long":
+			os_notify("Break Over!", "Get back to work! You've had long enough!")
