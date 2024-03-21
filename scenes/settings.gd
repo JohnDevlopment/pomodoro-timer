3668b1cdf090431d04bf3b1911a286a2ea1e0183
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-21 10:58:47 -0500

    [UPDATE] Add status label to settings

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -2,9 +2,11 @@ extends Control
 
 @onready var work_counter: Globals.IntField = %WorkCounter
 @onready var notif_timer: HBoxContainer = %NotifTimer
+@onready var settings_status: Globals.MessageLabel = %SettingsStatus
 
 func _ready() -> void:
 	_load_config()
+	settings_status.show_info("Configuration loaded")
 
 func _load_config():
 	work_counter.value = Config.timers_work_counter
@@ -14,6 +16,8 @@ func _on_save_pressed() -> void:
 	Config.timers_work_counter = work_counter.value
 	Config.os_notification_time = notif_timer.value
 	Config.save_config(Config.CONFIG_FILE)
+	settings_status.show_info("Configuration saved")
 
 func _on_reset_pressed() -> void:
 	_load_config()
+	settings_status.show_info("Settings reset")
