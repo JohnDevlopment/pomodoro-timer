8acba1cf5bd7fe19490071e80a806b6a8236f330
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-21 11:06:01 -0500

    [REFACTOR] Add documentation to Globals

--- scripts/autoload/Globals.gd
+++ scripts/autoload/Globals.gd
@@ -1,8 +1,10 @@
-## Globals
+## Globals and globally scoped helper functions.
 extends Node
 
-## The class behind [code]int_field.tscn[/code]
+## The class behind [code]int_field.tscn[/code].
+## Use for static typing.
 const IntField = preload("res://scenes/int_field/int_field.gd")
 
-## The class behind [code]message_label.tscn[/code]
+## The class behind [code]message_label.tscn[/code].
+## Use for static typing.
 const MessageLabel = preload("res://scenes/message_label.gd")
