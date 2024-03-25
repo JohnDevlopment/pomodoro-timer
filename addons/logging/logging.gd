f032ecedb3b058d805bccabd7db6d67aff9566af
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-25 15:20:43 -0500

    [UPDATE] Update Logging plugin

--- addons/logging/logging.gd
+++ addons/logging/logging.gd
@@ -2,7 +2,18 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
+	if not ProjectSettings.has_setting("jlogger/debug"):
+		ProjectSettings.set_setting("jlogger/debug", false)
+		ProjectSettings.set_initial_value("jlogger/debug", false)
+		ProjectSettings.add_property_info({
+			name = "jlogger/debug",
+			type = TYPE_BOOL
+		})
+		ProjectSettings.set_as_basic("jlogger/debug", true)
+	
+	add_autoload_singleton("LoggingConfig", "res://addons/logging/LoggingConfig.gd")
 	add_autoload_singleton("Logging", "res://addons/logging/logging_autoload.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("Logging")
+	remove_autoload_singleton("LoggingConfig")
--- addons/logging/test.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	var logger := Logger.new("test", Logger.Level.DEBUG)
-	logger.debug("This is a log.")
-	logger.info("This is a log.")
