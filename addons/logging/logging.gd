45fe84e47283c5dcb4df10d2585b621bce910668
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-03-25 20:13:40 -0500

    [UPDATE] Attempt at making Logging* singletons load once

--- addons/logging/logging.gd
+++ addons/logging/logging.gd
@@ -1,6 +1,9 @@
 @tool
 extends EditorPlugin
 
+func _has_autoload(name: String) -> bool:
+	return get_tree().root.has_node(name)
+
 func _enter_tree() -> void:
 	if not ProjectSettings.has_setting("jlogger/debug"):
 		ProjectSettings.set_setting("jlogger/debug", false)
@@ -11,8 +14,11 @@ func _enter_tree() -> void:
 		})
 		ProjectSettings.set_as_basic("jlogger/debug", true)
 	
-	add_autoload_singleton("LoggingConfig", "res://addons/logging/LoggingConfig.gd")
-	add_autoload_singleton("Logging", "res://addons/logging/logging_autoload.gd")
+	if not _has_autoload("LoggingConfig"):
+		add_autoload_singleton("LoggingConfig", "res://addons/logging/LoggingConfig.gd")
+	
+	if not _has_autoload("Logging"):
+		add_autoload_singleton("Logging", "res://addons/logging/logging_autoload.gd")
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("Logging")
