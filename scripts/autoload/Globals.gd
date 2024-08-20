59d7139dc47a730e5db43f849713d4761f7a1bef
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2024-08-20 14:48:16 -0500

    [ADD] Globals: time_to_seconds

--- scripts/autoload/Globals.gd
+++ scripts/autoload/Globals.gd
@@ -9,10 +9,22 @@ const IntField = preload("res://scenes/int_field/int_field.gd")
 ## Use for static typing.
 const MessageLabel = preload("res://scenes/message_label.gd")
 
+## Convert seconds into a time value, containing the
+## hours, minutes, and seconds.
 func seconds_to_time(sec: float) -> Dictionary:
 	var minutes := int(sec / 60.0)
 	return {
-		hours = sec / 3600.0,
+		hours = int(sec / 3600.0),
 		minutes = minutes,
 		seconds = sec - float(minutes) * 60.0
 	}
+
+## Convert the time value [param time] into seconds.
+## [param time] is a [Dictionary] with the keys
+## "hours", "minutes", and "seconds".
+func time_to_seconds(time: Dictionary) -> int:
+	return (
+		(time.hours * 60) *
+		(time.minutes * 60) +
+		time.seconds
+	)
